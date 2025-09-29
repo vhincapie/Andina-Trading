2f4b032291c151d710ae47e08e895f786b6d8f62
Viviana <vivianahincapie1610@gmail.com>
2025-09-29 13:47:57 -0500

    VH FR: Commit inicial

---
+++ Backend/servicio-autenticacion/src/main/java/co/edu/unbosque/foresta/ForestaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.foresta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestaApplication.class, args);
+	}
+
+}
